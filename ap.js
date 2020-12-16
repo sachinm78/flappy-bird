41087144b71b981110ec064866571b68048b5a0a
sachinm78 <sachinm78@gmail.com>
2020-12-16 10:40:54 -0600

    created event listener and startGame function to set up bird placement when the game starts

---
+++ ap.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded' , () => {
+    const bird = document.querySelector('.bird')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground-moving')
+
+    let birdLeft = 220
+    let birdBottom = 100
+
+    function startGame() {
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
+    }
+})
